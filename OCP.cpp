021f464b4421b8019d967ed5175b00026d5ac10d
gt951436 <tyagigarv95@gmail.com>
2025-12-06 15:50:43 +0530

    OCP violation

---
+++ OCP.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Product
+{
+public:
+    string name;
+    double price;
+    Product(string name, double price)
+    {
+        this->name = name;
+        this->price = price;
+    }
+};
+
+// SRP followed: ShoppingCart holds only 1 responsibility
+class ShoppingCart
+{
+private:
+    vector<Product *> products;
+
+public:
+    void addProduct(Product *p)
+    {
+        products.push_back(p);
+    }
+    const vector<Product *> &getProducts()
+    {
+        return products;
+    }
+    double calcTotalPrice()
+    {
+        double total = 0;
+        for (auto p : products)
+        {
+            total += p->price;
+        }
+        return total;
+    }
+};
+class ShoppingCartPrinter
+{
+private:
+    ShoppingCart *cart;
+
+public:
+    ShoppingCartPrinter(ShoppingCart *cart)
+    {
+        this->cart = cart;
+    }
+    void printInvoice()
+    {
+        cout << "Shopping Cart Invoice:\n";
+        for (auto p : cart->getProducts())
+        {
+            cout << p->name << " : $" << p->price << "\n";
+        }
+        cout << "Total Price: $" << cart->calcTotalPrice() << endl;
+    }
+};
+
+// OCP violated: Adding new storage types requires modifying this class
+class ShoppingCartStorageinDB
+{
+private:
+    ShoppingCart *cart;
+
+public:
+    ShoppingCartStorageinDB(ShoppingCart *cart)
+    {
+        this->cart = cart;
+    }
+    void saveToSQLDB()
+    {
+        cout << "Saving the shopping cart to SQL DB.." << endl;
+    }
+    void saveToMongoDB()
+    {
+        cout << "Saving the shopping cart to MongoDB.." << endl;
+    }
+    void saveToFile()
+    {
+        cout << "Saving the shopping cart to File.." << endl;
+    }
+};
+
+int main()
+{
+    ShoppingCart *cart = new ShoppingCart();
+    cart->addProduct(new Product("Book", 11.79));
+    cart->addProduct(new Product("Mouse", 15.49));
+
+    ShoppingCartPrinter *printer = new ShoppingCartPrinter(cart);
+    printer->printInvoice();
+
+    ShoppingCartStorageinDB *store = new ShoppingCartStorageinDB(cart);
+    store->saveToSQLDB();
+    store->saveToMongoDB();
+    store->saveToFile();
+
+    return 0; 
+}
