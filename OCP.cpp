71b56bd9fe8834a768b2c06c3b75c85b07355960
gt951436 <tyagigarv95@gmail.com>
2025-12-06 16:04:50 +0530

    OCP followed

--- OCP.cpp
+++ OCP.cpp
@@ -60,26 +60,65 @@ public:
     }
 };
 
-// OCP violated: Adding new storage types requires modifying this class
+// // OCP violated: Adding new storage types requires modifying this class
+// class ShoppingCartStorageinDB
+// {
+// private:
+//     ShoppingCart *cart;
+
+// public:
+//     ShoppingCartStorageinDB(ShoppingCart *cart)
+//     {
+//         this->cart = cart;
+//     }
+//     void saveToSQLDB()
+//     {
+//         cout << "Saving the shopping cart to SQL DB.." << endl;
+//     }
+//     void saveToMongoDB()
+//     {
+//         cout << "Saving the shopping cart to MongoDB.." << endl;
+//     }
+//     void saveToFile()
+//     {
+//         cout << "Saving the shopping cart to File.." << endl;
+//     }
+// };
+
+// OCP followed: Each storage type has its own class
+
+// Abstract base class
 class ShoppingCartStorageinDB
 {
 private:
     ShoppingCart *cart;
 
 public:
-    ShoppingCartStorageinDB(ShoppingCart *cart)
-    {
-        this->cart = cart;
-    }
-    void saveToSQLDB()
+    virtual void saveToDB(ShoppingCart *cart) = 0; // pure virtual fn
+};
+// SQL storage class
+class storeToSQLDB : public ShoppingCartStorageinDB
+{
+public:
+    void saveToDB(ShoppingCart *cart) override
     {
         cout << "Saving the shopping cart to SQL DB.." << endl;
     }
-    void saveToMongoDB()
+};
+// MongoDB storage class
+class storeToMongoDB : public ShoppingCartStorageinDB
+{
+public:
+    void saveToDB(ShoppingCart *cart) override
     {
         cout << "Saving the shopping cart to MongoDB.." << endl;
     }
-    void saveToFile()
+};
+// File storage class
+class storeToFile : public ShoppingCartStorageinDB
+{
+public:
+    void saveToDB(ShoppingCart *cart) override
     {
         cout << "Saving the shopping cart to File.." << endl;
     }
@@ -94,10 +133,12 @@ int main()
     ShoppingCartPrinter *printer = new ShoppingCartPrinter(cart);
     printer->printInvoice();
 
-    ShoppingCartStorageinDB *store = new ShoppingCartStorageinDB(cart);
-    store->saveToSQLDB();
-    store->saveToMongoDB();
-    store->saveToFile();
+    ShoppingCartStorageinDB *store1 = new storeToSQLDB();
+    store1->saveToDB(cart);
+    ShoppingCartStorageinDB *store2 = new storeToMongoDB();
+    store2->saveToDB(cart);
+    ShoppingCartStorageinDB *store3 = new storeToFile();
+    store3->saveToDB(cart);
 
-    return 0; 
+    return 0;
 }
