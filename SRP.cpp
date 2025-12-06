137bb62fd9f66f7d7d8f36e840cf9dfceae240ae
gt951436 <tyagigarv95@gmail.com>
2025-12-06 14:44:27 +0530

    SRP followed

--- SRP.cpp
+++ SRP.cpp
@@ -15,6 +15,44 @@ public:
 };
 
 // SRP violation: ShoppingCart holds multiple responsibilities
+// class ShoppingCart
+// {
+// private:
+//     vector<Product *> products;
+
+// public:
+//     void addProduct(Product *p)
+//     {
+//         products.push_back(p);
+//     }
+//     const vector<Product *> &getProducts()
+//     {
+//         return products;
+//     }
+//     double calcTotalPrice()
+//     {
+//         double total = 0;
+//         for (auto p : products)
+//         {
+//             total += p->price;
+//         }
+//         return total;
+//     }
+//     void printInvoice()
+//     {
+//         cout << "Shopping Cart Invoice:\n";
+//         for (auto p : products)
+//         {
+//             cout << p->name << " : $" << p->price << "\n";
+//         }
+//     }
+//     void saveToDB()
+//     {
+//         cout << "Saving the shopping cart to database.." << endl;
+//     }
+// };
+
+// SRP followed: ShoppingCart holds only 1 responsibility
 class ShoppingCart
 {
 private:
@@ -38,13 +76,36 @@ public:
         }
         return total;
     }
+};
+class ShoppingCartPrinter
+{
+private:
+    ShoppingCart *cart;
+
+public:
+    ShoppingCartPrinter(ShoppingCart *cart)
+    {
+        this->cart = cart;
+    }
     void printInvoice()
     {
         cout << "Shopping Cart Invoice:\n";
-        for (auto p : products)
+        for (auto p : cart->getProducts())
         {
             cout << p->name << " : $" << p->price << "\n";
         }
+        cout << "Total Price: $" << cart->calcTotalPrice() << endl;
+    }
+};
+class ShoppingCartStorageinDB
+{
+private:
+    ShoppingCart *cart;
+
+public:
+    ShoppingCartStorageinDB(ShoppingCart *cart)
+    {
+        this->cart = cart;
     }
     void saveToDB()
     {
@@ -58,7 +119,11 @@ int main()
     cart->addProduct(new Product("Book", 11.79));
     cart->addProduct(new Product("Mouse", 15.49));
 
-    cart->printInvoice();
-    cart->saveToDB();
+    ShoppingCartPrinter *printer = new ShoppingCartPrinter(cart);
+    printer->printInvoice();
 
+    ShoppingCartStorageinDB *store = new ShoppingCartStorageinDB(cart);
+    store->saveToDB();
+
+    return 0; 
 }
