a92e9f5bff822b248229d6dd4774258cf8fca3a9
gt951436 <tyagigarv95@gmail.com>
2025-12-06 19:35:14 +0530

    LSP violation solve by wrong method

--- LSP.cpp
+++ LSP.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 using namespace std;
 
-// LSP violated
 class Account
 {
 public:
@@ -89,6 +88,7 @@ public:
     }
 };
 
+// LSP Violating Client
 class BankClient
 {
 private:
@@ -118,6 +118,39 @@ public:
     }
 };
 
+// LSP solved : wrong method : checking account type by clinet
+class BankClient
+{
+private:
+    vector<Account *> accounts;
+
+public:
+    BankClient(vector<Account *> accounts)
+    {
+        this->accounts = accounts;
+    }
+    void processTranactions()
+    {
+        for (Account *acc : accounts)
+        {
+            acc->deposit(1000); // all accs allow deposit
+            if (typeid(*acc) == typeid(fixedTermAccount))
+            {
+                cout << "Skipping transactions for fixed term account." << endl;
+                continue;
+            }
+            // assuming all accs support withdrawl (LSP Violation)
+            try
+            {
+                acc->withdraw(500);
+            }
+            catch (const logic_error &e)
+            {
+                cout << "Exception: " << e.what() << '\n';
+            }
+        }
+    }
+};
 int main()
 {
     vector<Account *> accounts;
