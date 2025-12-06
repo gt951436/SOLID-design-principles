fbee8f0065703cab9fc4826ec8a8bf936c6f588a
gt951436 <tyagigarv95@gmail.com>
2025-12-06 13:55:34 +0530

    SRP violation demostration

---
+++ SRP.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Product
+{
+public:
+    string name;
+    double price;
+    Product(string name, double price)
+    {
+        this->name = name;
+        this->price = price;
+    }
+};
+
+// SRP violation: ShoppingCart holds multiple responsibilities
+class ShoppingCart
+{
+private:
+    vector<Product *> products;
+
+public:
+    void addProduct(Product *p)
+    {
+        products.push_back(p);
+    }
+    const vector<Product *> &getProducts()
+    {
+        return products;
+    }
+    double calcTotalPrice()
+    {
+        double total = 0;
+        for (auto p : products)
+        {
+            total += p->price;
+        }
+        return total;
+    }
+    void printInvoice()
+    {
+        cout << "Shopping Cart Invoice:\n";
+        for (auto p : products)
+        {
+            cout << p->name << " : $" << p->price << "\n";
+        }
+    }
+    void saveToDB()
+    {
+        cout << "Saving the shopping cart to database.." << endl;
+    }
+};
+
+int main()
+{
+    ShoppingCart *cart = new ShoppingCart();
+    cart->addProduct(new Product("Book", 11.79));
+    cart->addProduct(new Product("Mouse", 15.49));
+
+    cart->printInvoice();
+    cart->saveToDB();
+
+}
